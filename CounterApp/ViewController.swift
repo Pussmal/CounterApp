4fa6e244cbb3348c5c21a99cb42af15e159366ca
Motorin Aleksey <aleksey.motorin2015@yandex.ru>
2022-09-29 14:38:11 +0300

    Initial Commit

---
+++ CounterApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CounterApp
+//
+//  Created by Алексей Моторин on 29.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
